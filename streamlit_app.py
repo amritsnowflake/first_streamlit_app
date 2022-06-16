db6d967f4c2d6f3239649f413f8981909ed0448a
Amrit Raj Karn <107607827+amritsnowflake@users.noreply.github.com>
2022-06-16 12:26:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
+stearmlit.text("Omega 3 & Blueberry Oatmeal")
+stearmlit.text("Kale, Spinach & Rocket Smoothie")
+stearmlit.text("Hard-Boiled Free-Range Egg")
